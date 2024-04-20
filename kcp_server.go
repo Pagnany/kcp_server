b3383271d94b861e62325bbe9d5ae950bf76c54f
Eric Schmale <eric.schmale@gmx.de>
2024-04-20 21:48:03 +0200

    diesdas

--- kcp_server.go
+++ kcp_server.go
@@ -20,6 +20,12 @@ type Mitglied struct {
 	Nickname string
 }
 
+type Veranstaltung struct {
+	Id          int
+	Bezeichnung string
+	Datum       string
+}
+
 func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	tmpl := template.Must(template.ParseFiles("html/home.html"))
 	tmpl.Execute(w, nil)
@@ -32,11 +38,16 @@ func mitglieder(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func strafen(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Strafen")
+	strafen := get_veranstaltungen()
+	tmpl := template.Must(template.ParseFiles("html/strafen.html"))
+	tmpl.Execute(w, strafen)
 }
 
-func clicked(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "you clicked")
+func strafenzeitraum(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	r.ParseForm()
+	von_datum := r.PostFormValue("von_datum")
+	bis_datum := r.PostFormValue("bis_datum")
+	fmt.Fprintf(w, "Von: %s, Bis: %s", von_datum, bis_datum)
 }
 
 func main() {
@@ -44,19 +55,19 @@ func main() {
 	router.GET("/", home)
 	router.GET("/mitglieder", mitglieder)
 	router.GET("/strafen", strafen)
-	router.GET("/clicked", clicked)
+	router.POST("/strafenzeitraum", strafenzeitraum)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
 
-func get_mitglieder() []Mitglied {
+func connect_to_db() {
 	// Capture connection properties.
 	cfg := mysql.Config{
 		User:   "root",
 		Passwd: "",
 		Net:    "tcp",
 		Addr:   "127.0.0.1:3306",
-		DBName: "testo",
+		DBName: "kcp",
 	}
 
 	// Get a database handle.
@@ -65,7 +76,10 @@ func get_mitglieder() []Mitglied {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+}
+
+func get_mitglieder() []Mitglied {
+	connect_to_db()
 
 	// Query the database.
 	rows, err := db.Query("SELECT * FROM mitglieder")
@@ -86,5 +100,28 @@ func get_mitglieder() []Mitglied {
 		}
 		mg = append(mg, Mitglied{id, name, vname, nickname})
 	}
+	db.Close()
 	return mg
+}
+func get_veranstaltungen() []Veranstaltung {
+	connect_to_db()
+	rows, err := db.Query("SELECT * FROM veranstaltungen")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	vers := []Veranstaltung{}
+	for rows.Next() {
+		var id int
+		var bezeichnung string
+		var datum string
+		err := rows.Scan(&id, &bezeichnung, &datum)
+		if err != nil {
+			log.Fatal(err)
+		}
+		vers = append(vers, Veranstaltung{id, bezeichnung, datum})
+	}
+	db.Close()
+	return vers
 }
